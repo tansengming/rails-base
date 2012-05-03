6c73ce196ac9bf68a8b329442a51a1303373bbbc
SengMing Tan <tansengming@gmail.com>
2012-05-03 12:35:50 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'pg'
+gem 'jquery-rails'
+gem 'activeadmin'
+gem 'sass-rails'
+gem 'haml'
+gem 'twitter-bootstrap-rails'
+
+group :development, :test do
+  gem 'kumade'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails', :require => false
+  gem 'unicorn'
+  gem 'heroku'
+end
+
+group :assets do
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBase::Application.config.secret_token = 'b3d179dc0a13c5a9dce314423d5711421c380447968ea8393d67d8f629cd11ff047430c22bde586ae1f3917f6e9e1d9702b4507efda0062792492e18507bedc7'
