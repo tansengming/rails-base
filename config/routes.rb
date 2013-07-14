41c5f66ca37e5ecf6aaf6d6c95661a950b03e020
SengMing Tan <tansengming@gmail.com>
2013-07-14 11:40:18 +1000

    adds /user/edit to test out dailycred

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ RailsBase::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :super_admins, ActiveAdmin::Devise.config.merge(:path => :super_admins)
 
+  resource :user, only: :edit
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
