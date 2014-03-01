ab0aadfe728bb350eafc25ea6ba570f0c909c1e7
SengMing Tan <tansengming@gmail.com>
2014-03-01 12:54:22 +1100

    fixing all rails 4 warning

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,8 +3,4 @@ class AdminUser < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
 end
