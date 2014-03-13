c4748a7d3550dea32c09749cb4cb33bcb4d9a2e3
SengMing Tan <tansengming@gmail.com>
2014-03-14 08:40:35 +1100

    updates config initialisers for rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsBase::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBase::Application.config.secret_key_base = 'b7655c365d2dea4fbb6a3ddd3dbe3370dc96fcd3c7b3dbc545447c9b489218b4a3fea4939d0a0cfe878a5fe21a961dcc5bd0e8e6cfc4e49dd7d230da9af98a91'
