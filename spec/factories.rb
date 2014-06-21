266c599da68684814d5c492a5d67e5fb674d861f
SengMing Tan <tansengming@gmail.com>
2014-06-21 11:01:42 +1000

    fixes factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :admin_user do
     sequence(:email) { |n| "admin-#{n}@example.com" }
-    password "foo1234"
+    password "foo12345"
   end
 
   factory :super_admin do
     sequence(:email) { |n| "super-admin-#{n}@example.com" }
-    password "foo1234"
+    password "foo12345"
   end
 
   factory :user do
