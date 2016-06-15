0a5baf3bbf6ffff99d8e0feac3e985af77c7ac6d
SengMing Tan <tansengming@gmail.com>
2016-06-15 13:10:24 +0200

    use App.heap_app_id

--- config/app.rb
+++ config/app.rb
@@ -1,5 +1,5 @@
 class App < Configurable # :nodoc:
   # Settings in config/app/* take precedence over those specified here.
-  config.name = Rails.application.class.parent.name
-  config.heap = false
+  config.name        = Rails.application.class.parent.name
+  config.heap_app_id = ENV['HEAP_APP_ID']
 end
