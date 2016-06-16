b877b4109f175f43fdbdbe26a9e6d6d4bcd66eb8
SengMing Tan <tansengming@gmail.com>
2016-06-16 13:38:31 +0200

    load heap in default layout

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,6 +10,15 @@ describe UsersController do
     context 'after logging in' do
       before { login_as user }
       it { should be_success }
+
+      context 'when heap ID is set' do
+        render_views
+        before { allow(App).to receive(:heap_app_id?) { true } }
+
+        it 'should have heap' do
+          expect(subject.body).to include 'heap.identify'
+        end
+      end
     end
 
     context 'if not logged in' do
