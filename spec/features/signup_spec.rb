d8a74a893f2ababd1fda1186bfc17fc62eaff823
SengMing Tan <tansengming@gmail.com>
2016-06-27 13:51:02 +0200

    makes sure sign up spec actually works

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe 'signup' do
   it 'should show signup page' do
     visit '/auth'
+    expect(page.status_code).to eq 200
   end
 
   it 'should show password reset' do
     visit "/auth/reset_password"
+    expect(page.status_code).to eq 200
   end
 end
