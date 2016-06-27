c92ee34f0cb364d2e5c23d24363f79582082d68c
SengMing Tan <tansengming@gmail.com>
2016-06-27 14:29:02 +0200

    fixes spec

--- spec/mailers/notifier_mailer_spec.rb
+++ spec/mailers/notifier_mailer_spec.rb
@@ -8,7 +8,7 @@ describe NotifierMailer do
     before     { stub }
     let(:stub) {
       stub_request(:post, "https://api.sendwithus.com/api/v1/send").
-               with(:body => "{\"email_id\":\"ID-CODE-FROM-SEND-WITH-US\",\"recipient\":{\"address\":\"#{user.email}\"},\"email_data\":{\"recipient\":null},\"esp_account\":\"\",\"version_name\":\"\",\"locale\":\"en-US\"}").
+               with(:body => "{\"email_id\":\"ID-CODE-FROM-SEND-WITH-US\",\"recipient\":{\"address\":\"#{user.email}\"},\"email_data\":{\"recipient\":null},\"esp_account\":\"\",\"version_name\":\"\",\"locale\":\"\"}").
                to_return(:status => 200, :body => "", :headers => {})
     }
 
