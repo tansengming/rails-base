72fc80e0cef925f6d32608f1337106e82496182e
SengMing Tan <tansengming@gmail.com>
2016-10-26 16:32:23 -0400

    spec admin user password update

--- spec/features/super_admin_spec.rb
+++ spec/features/super_admin_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe 'Admin' do
   subject { page }
-  let!(:admin) { create(:admin_user, :password => '12345678', email: 'an_admin@example.com') }
+  let(:admin) { create(:admin_user, :password => '12345678', email: 'an_admin@example.com') }
   let(:super_admin) { create(:super_admin, password: 'super12345678') }
+
+  before { admin }
 
   describe 'login' do
     before do
@@ -14,12 +16,29 @@ describe 'Admin' do
     end
     its(:current_path) { should == '/super_admins' }
 
-    describe 'walking around the place' do
-      it 'should not have an exception' do
-        click_link 'Admin Users'
-        expect(page).to have_content('an_admin@example.com')
-        click_link 'New Admin User'
-        click_link 'Logout'
+    describe 'index page' do
+      before { click_link 'Admin Users' }
+
+      it { should have_content('an_admin@example.com') }
+
+      describe 'updating the admin user' do
+        before do
+          first('.table_actions').click_link('Edit')
+          fill_in 'Email', with: 'new_email@example.com'
+          click_button 'Update Admin user'
+        end
+
+        it 'should update the admin' do
+          expect(page).to have_content('Admin user was successfully updated.')
+          expect(page).to have_content('new_email@example.com')
+        end
+      end
+
+      describe 'walking around' do
+        it 'should not crash' do
+          click_link 'New Admin User'
+          click_link 'Logout'
+        end
       end
     end
   end
