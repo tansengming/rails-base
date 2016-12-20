dc106a0468f174815180cc9ced22e3ffada5dd32
SengMing Tan <tansengming@gmail.com>
2016-12-20 16:42:00 -0500

    moves heap stuff to user walk spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,15 +10,6 @@ describe UsersController do
       before { sign_in user }
       it { should be_success }
 
-      skip 'when heap ID is set' do
-        render_views
-        before { allow(App).to receive(:heap_app_id?) { true } }
-
-        it 'should have heap' do
-          expect(subject.body).to include 'heap.identify'
-        end
-      end
-
       context 'and user is deleted' do
         before { user.delete }
         it { should be_redirect }
