068449a77239b3f04e40f1dd65008773c2a8c2d2
SengMing Tan <tansengming@gmail.com>
2017-01-11 14:46:24 -0500

    coverage filter for spec folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
 require 'webmock/rspec'
 WebMock.disable_net_connect! allow_localhost: true, allow: 'codeclimate.com'
