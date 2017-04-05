baf39fd1a3aafb3e4500a62f8861ce98aa554249
SengMing Tan <tansengming@gmail.com>
2017-04-05 10:41:25 -0400

    fixes rollbar config

--- config/app/test.rb
+++ config/app/test.rb
@@ -1,4 +1,5 @@
 App.configure do
   # Settings specified here will take precedence over those in config/app.rb
-  config.heap_app_id = 'HEAP_APP_ID'
+  config.heap_app_id      = 'HEAP_APP_ID'
+  config.rollbar_enabled  = false
 end
