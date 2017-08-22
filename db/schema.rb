c2b630a89bbd87869d01ffe0bdd710954cb110b7
SengMing Tan <tansengming@gmail.com>
2017-08-22 15:44:46 -0400

    schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170404211412) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.datetime "active_until"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
