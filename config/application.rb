6d042eda0617cd5d7c4bb84ed6404fd71c47e232
SengMing Tan <tansengming@gmail.com>
2017-08-22 16:15:33 -0400

    adds payment screen

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RailsBase
     config.active_job.queue_adapter = :sidekiq
     config.active_job.queue_name_prefix = "rails5_#{Rails.env}"
 
+    config.stripe.publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
