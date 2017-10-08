858340eff9582306be3524f8b76164af55f8911c
SengMing Tan <tansengming@gmail.com>
2017-10-08 15:48:04 -0400

    fixes spec

--- spec/features/payment_flow_spec.rb
+++ spec/features/payment_flow_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Payment Flow' do
   def fill_and_submit_sign_up_form
     visit '/'
-    click_on 'Sign Up'
+    within '#sign_up_or_log_in' do
+      click_on 'Sign Up'
+    end
     fill_in 'Email', with: 'email@example.com'
     fill_in 'Password', with: '12345678'
     fill_in 'Password confirmation', with: '12345678'
