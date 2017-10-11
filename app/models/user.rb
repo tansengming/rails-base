dea5994dded372880c7978acba02453602dbcbbb
SengMing Tan <tansengming@gmail.com>
2017-10-11 16:34:06 -0400

    adds devise lockable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :trackable,
+          :validatable
 
   has_one :stripe_customer
 end
