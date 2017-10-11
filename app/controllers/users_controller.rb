092ee8da40c813c404979af2a8c48c7d30b376df
SengMing Tan <tansengming@gmail.com>
2017-10-11 16:19:20 -0400

    parans

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def edit
-    redirect_to(plans_path) unless policy(User).edit?
+    redirect_to plans_path unless policy(User).edit?
   end
 end
