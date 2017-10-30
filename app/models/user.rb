c982d698dbc6229a56ad26590aa7c0d7a6881474
SengMing Tan <tansengming@gmail.com>
2017-10-30 17:48:59 -0400

    lets controller handle save errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
 
   has_one :stripe_customer
 
+  validates :email, uniqueness: true
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-      user.email    = auth.info.email
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
     end
   end
 end
