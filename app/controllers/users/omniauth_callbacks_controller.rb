dc113631ca3fec4b8f893a9b4291c253fc6be7cc
SengMing Tan <tansengming@gmail.com>
2017-10-30 16:58:16 -0400

    fixes flash messages

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,12 +6,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Auth0") if is_navigational_format?
     else
-      flash[:error] = 'There was a problem, please try again.'
+      flash.alert = 'There was a problem, please try again.'
       redirect_to root_path
     end
   end
 
   def failure
+    @error = params[:error_description]
+    flash.alert = "There was a problem: '#{@error}'. Please try again."
+
     redirect_to root_path
   end
 end
