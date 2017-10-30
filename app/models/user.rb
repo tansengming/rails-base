17b328717a059afe1d71468abda71b43cbbaf6f3
SengMing Tan <tansengming@gmail.com>
2017-10-30 15:46:39 -0400

    removes devise-db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise  :database_authenticatable,
-          :omniauthable,
+  devise  :omniauthable,
           :omniauth_providers => [:auth0]
 
   has_one :stripe_customer
@@ -11,7 +8,6 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email    = auth.info.email
-      user.password = Devise.friendly_token[0,20]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   devise_for :admin_users,  ActiveAdmin::Devise.config
   devise_for :super_admins, ActiveAdmin::Devise.config.merge(path: :super_admins)
 
