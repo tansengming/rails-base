a3afbd28524adcb0ce01fb6f9bd2bd72c1d29ba7
SengMing Tan <tansengming@gmail.com>
2017-10-30 15:45:37 -0400

    raise exception if user is not saved

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def auth0
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted? || @user.save
+    if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Auth0") if is_navigational_format?
     else
-      flash[:error] = 'There was a problem'
+      flash[:error] = 'There was a problem, please try again.'
+      redirect_to root_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email    = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
