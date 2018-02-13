cade41e97fd93488079916bc22aa36d55a69cb39
SengMing Tan <tansengming@gmail.com>
2018-02-12 19:55:36 -0500

    adds chrome options

--- spec/support/chrome_headless.rb
+++ spec/support/chrome_headless.rb
@@ -5,8 +5,11 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
+  headless_options = %w(headless window-size=1200,1100 disable-gpu)
+  headful_options  = %w(window-size=1200,1100)
+
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless window-size=1200,1100 disable-gpu) }
+    chromeOptions: { args: headless_options }
   )
 
   Capybara::Selenium::Driver.new  app,
