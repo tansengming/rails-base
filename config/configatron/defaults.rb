5d345043a454bf40e41fbd6d35bd80057658357e
SengMing Tan <tansengming@gmail.com>
2018-03-10 16:04:19 -0500

    moves all global vars to configatron

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,8 +1,13 @@
 # Put all your default configatron settings here.
-configatron.redis_url            = ENV['REDIS_URL']
-configatron.rollbar.access_token = Rails.application.secrets.rollbar_post_client_item_access_token || ENV['ROLLBAR_ACCESS_TOKEN']
-configatron.rollbar.enabled      = true
-configatron.auth0.callback_url   = ENV['AUTH0_CALLBACK_URL']
-configatron.auth0.client_id      = Rails.application.secrets.auth0_client_id || ENV['AUTH0_CLIENT_ID']
-configatron.auth0.client_secret  = Rails.application.secrets.auth0_client_secret || ENV['AUTH0_CLIENT_SECRET']
-configatron.auth0.client_domain  = Rails.application.secrets.auth0_client_domain || ENV['AUTH0_DOMAIN']
+RAILS_SECRETS = Rails.application.secrets
+
+configatron.auth0.callback_url     = ENV['AUTH0_CALLBACK_URL']
+configatron.auth0.client_id        = RAILS_SECRETS.auth0_client_id || ENV['AUTH0_CLIENT_ID']
+configatron.auth0.client_secret    = RAILS_SECRETS.auth0_client_secret || ENV['AUTH0_CLIENT_SECRET']
+configatron.auth0.client_domain    = RAILS_SECRETS.auth0_client_domain || ENV['AUTH0_DOMAIN']
+configatron.heap.app_id            = RAILS_SECRETS.heap_app_id || ENV['HEAP_APP_ID']
+configatron.redis_url              = ENV['REDIS_URL']
+configatron.rollbar.access_token   = RAILS_SECRETS.rollbar_post_client_item_access_token || ENV['ROLLBAR_ACCESS_TOKEN']
+configatron.rollbar.enabled        = true
+configatron.stripe.secret_key      = RAILS_SECRETS.stripe_secret_key || ENV['STRIPE_SECRET_KEY']
+configatron.stripe.publishable_key = RAILS_SECRETS.stripe_publishable_key || ENV['STRIPE_PUBLISHABLE_KEY']
