d138120b46699ffb72b69420e0ff07ddc0c87ca6
SengMing Tan <tansengming@gmail.com>
2018-10-02 17:15:31 -0400

    re-adds devise migrations

---
+++ db/migrate/20181002210823_readds_devise_cols.rb
@@ -0,0 +1,31 @@
+class ReaddsDeviseCols < ActiveRecord::Migration[5.2]
+  def up
+    ## Database authenticatable
+    add_column :users, :encrypted_password, :string, null: false, default: ""
+
+    ## Recoverable
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+
+    ## Rememberable
+    add_column :users, :remember_created_at, :datetime
+
+    ## Lockable
+    add_column :users, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :users, :unlock_token, :string # Only if unlock strategy is :email or :both
+    add_column :users, :locked_at, :datetime
+
+    # Uncomment below if timestamps were not included in your original model.
+    # t.timestamps null: false
+
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
