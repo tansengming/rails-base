f9449babf473dad663e68cc3c92c4409337c8c88
SengMing Tan <tansengming@gmail.com>
2018-10-02 17:31:12 -0400

    factory bot fixes

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :admin_user do
     sequence(:email) { |n| "admin-#{n}@example.com" }
-    password "foo12345"
+    password { "foo12345" }
   end
 
   factory :super_admin do
     sequence(:email) { |n| "super-admin-#{n}@example.com" }
-    password "foo12345"
+    password { "foo12345" }
   end
 
   factory :user do
     sequence(:email) { |n| "user-#{n}@example.com" }
-    password "foo12345"
+    password { "foo12345" }
   end
 end
