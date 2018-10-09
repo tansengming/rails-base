656a19b320163de206852ca8869c82f52a41fcb8
SengMing Tan <tansengming@gmail.com>
2018-10-09 18:37:13 -0400

    adds devise activejob integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
   has_one   :stripe_customer, -> { stripe_customers.order('id desc') }, class_name: 'RemoteKey', as: :remoteable
 
   validates :email, uniqueness: true, presence: true
+
+  # https://github.com/plataformatec/devise#activejob-integration
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
