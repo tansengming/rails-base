f1c13a334a04b3c577caff26b6099bfa7ec6d5b8
SengMing Tan <tansengming@gmail.com>
2018-10-23 14:28:14 -0400

    Use nested module/class definitions instead of compact style. thanks code climate

--- app/controllers/stripetk/payments_controller.rb
+++ app/controllers/stripetk/payments_controller.rb
@@ -1,36 +1,38 @@
-class Stripetk::PaymentsController < ApplicationController
-  rescue_from Stripe::InvalidRequestError, with: :retry_payment
-  before_action :authenticate_user!
-  helper_method :selected_plan
+module Stripetk
+  class PaymentsController < ApplicationController
+    rescue_from Stripe::InvalidRequestError, with: :retry_payment
+    before_action :authenticate_user!
+    helper_method :selected_plan
 
-  def new
-    if selected_plan
-      render
-    else
-      redirect_to stripetk_plans_path, flash: { alert: 'Please select a plan before continuing.' }
+    def new
+      if selected_plan
+        render
+      else
+        redirect_to stripetk_plans_path, flash: { alert: 'Please select a plan before continuing.' }
+      end
     end
-  end
 
-  def create
-    payment_params = params.slice(:stripeToken, :plan)
-    @form = PaymentForm.new(OpenStruct.new)
+    def create
+      payment_params = params.slice(:stripeToken, :plan)
+      @form = PaymentForm.new(OpenStruct.new)
 
-    if @form.validate(payment_params)
-      Payments::Create.(payment_params[:stripeToken], payment_params[:plan], current_user)
+      if @form.validate(payment_params)
+        Payments::Create.(payment_params[:stripeToken], payment_params[:plan], current_user)
 
-      redirect_to page_path('thanks')
-    else
-      retry_payment
+        redirect_to page_path('thanks')
+      else
+        retry_payment
+      end
     end
-  end
 
-  private
+    private
 
-  def retry_payment
-    redirect_to(new_stripetk_payment_path, flash: { alert: 'There was a problem with the payment, please try again' }) && return
-  end
+    def retry_payment
+      redirect_to(new_stripetk_payment_path, flash: { alert: 'There was a problem with the payment, please try again' }) && return
+    end
 
-  def selected_plan
-    Stripe::Plans.all.find { |plan| plan.id.to_s == params[:plan] }
+    def selected_plan
+      Stripe::Plans.all.find { |plan| plan.id.to_s == params[:plan] }
+    end
   end
 end
--- app/controllers/stripetk/plans_controller.rb
+++ app/controllers/stripetk/plans_controller.rb
@@ -1,7 +1,9 @@
-class Stripetk::PlansController < ApplicationController
-  before_action :authenticate_user!
+module Stripetk
+  class PlansController < ApplicationController
+    before_action :authenticate_user!
 
-  def index
-    @plans = Stripe::Plans.all.select { |plan| plan.metadata[:display] }
+    def index
+      @plans = Stripe::Plans.all.select { |plan| plan.metadata[:display] }
+    end
   end
 end
