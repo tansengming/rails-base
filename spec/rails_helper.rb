44e4052e77401a7791e1e9bbea799c5e58273f0b
SengMing Tan <tansengming@gmail.com>
2018-10-29 12:45:40 -0400

    removes comment about stripe mock server

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,11 +30,6 @@ end
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-# Stripe Mock Server
-# https://github.com/rebelidealist/stripe-ruby-mock#running-the-mock-server
-# require 'thin'
-# StripeMock.spawn_server
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
