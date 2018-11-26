a00b7388c85c26a753f919e8191ffec6df5a61b3
SengMing Tan <tansengming@gmail.com>
2018-11-26 13:34:22 -0500

    removes reform forms

--- app/forms/payment_form.rb
+++
@@ -1,8 +0,0 @@
-class PaymentForm < Reform::Form
-  property :stripeToken
-  property :plan
-
-  validates :stripeToken, presence: true
-  validates :plan, presence: true
-  validates :plan, inclusion: { in: Stripe::Plans.all.select { |plan| plan.metadata[:enable] }.map(&:id).map(&:to_s) }
-end
