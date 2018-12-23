e5ac01de9265e6e3c5fb478494a2654d886904e2
SengMing Tan <tansengming@gmail.com>
2018-12-23 16:51:31 -0500

    expires fragment cache after 90 minutes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, ENV.fetch('REDIS_URL')
+  config.cache_store = :redis_store, ENV.fetch('REDIS_URL'), { expires_in: 90.minutes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
