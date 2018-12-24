6905c6dbf6c2a35dad8726b478acc9afbb93229d
SengMing Tan <tansengming@gmail.com>
2018-12-24 12:36:52 -0500

    removes configatron.redis_url

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -2,6 +2,5 @@
 RAILS_SECRETS = Rails.application.secrets
 
 configatron.heap.app_id            = RAILS_SECRETS.heap_app_id || ENV['HEAP_APP_ID']
-configatron.redis_url              = ENV['REDIS_URL']
 configatron.rollbar.access_token   = RAILS_SECRETS.rollbar_post_client_item_access_token || ENV['ROLLBAR_ACCESS_TOKEN']
 configatron.rollbar.enabled        = true
