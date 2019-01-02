807d876028e3a4dac1e7214eabcfb330d2340a68
SengMing Tan <tansengming@gmail.com>
2019-01-02 10:42:55 -0500

    rubocop

--- spec/system/ups_controller_spec.rb
+++ spec/system/ups_controller_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UpsController do
   describe '/up' do
-    context 'json' do
-      subject { visit '/up.json' }
+    context 'when using json' do
+      subject(:get_json) { visit '/up.json' }
 
       it 'should return ok' do
-        subject
+        get_json
         expect(page.body).to eq '{"status":"ok"}'
       end
     end
 
-    context 'html' do
-      subject { visit '/up' }
+    context 'when using html' do
+      subject(:get_html) { visit '/up' }
 
       it 'should raise an error' do
-        expect { subject }.to raise_error ActionController::UnknownFormat
+        expect { get_html }.to raise_error ActionController::UnknownFormat
       end
     end
   end
