4b490f9b8925bbec802d6351652d06088500109b
SengMing Tan <tansengming@gmail.com>
2019-01-02 11:14:02 -0500

    rubocop

--- spec/mailers/notifier_mailer_spec.rb
+++ spec/mailers/notifier_mailer_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe NotifierMailer do
   let(:user) { create :user }
 
   describe '#welcome' do
-    subject    { described_class.welcome(user).deliver }
-    before     { stub }
+    subject(:deliver_mail) { described_class.welcome(user).deliver }
+
+    before { stub }
+
     let(:stub) do
       stub_request(:post, 'https://api.sendwithus.com/api/v1/send')
         .with(body: "{\"email_id\":\"ID-CODE-FROM-SEND-WITH-US\",\"recipient\":{\"address\":\"#{user.email}\"},\"esp_account\":\"\",\"version_name\":\"\",\"locale\":\"\"}")
@@ -13,7 +15,8 @@ RSpec.describe NotifierMailer do
     end
 
     it 'should not crash' do
-      expect { subject }.not_to raise_error
+      deliver_mail
+      expect(stub).to have_been_requested
     end
   end
 end
