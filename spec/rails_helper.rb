6becbf3aaef94e4381f29f7f265ce52788783987
SengMing Tan <tansengming@gmail.com>
2019-01-21 15:25:04 -0500

    auto load jobs helpers on jobs specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include ActiveJob::TestHelper, type: :job
 
   # https://medium.com/table-xi/a-quick-guide-to-rails-system-tests-in-rspec-b6e9e8a8b5f6
   config.before(:each, type: :system) do
