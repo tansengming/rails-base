0fcffce1c56fc439245804e59e36dd76f5ab5178
SengMing Tan <tansengming@gmail.com>
2020-11-28 10:59:29 -0500

    removes goods only atrributes for product

--- config/stripe/products.rb
+++ config/stripe/products.rb
@@ -4,7 +4,5 @@ Stripe.product :test_product_no_type do |product|
   product.attributes  = ['size', 'gender']
   product.metadata    = {:number_of_awesome_things => 5}
   product.statement_descriptor = 'PRIMO'
-  product.caption     = 'So good it is Primo'
   product.description = 'One step beyond supreme'
-  product.shippable   = false
 end
